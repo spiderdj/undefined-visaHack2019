3079a36fbf96e3100e67acaede7aa32712b1baa1
jessy <janlie.jessy@outlook.com>
2019-01-20 14:03:24 +0000

    change shop

--- ui/undefinedApp/src/app/ui/ui.module.ts
+++ ui/undefinedApp/src/app/ui/ui.module.ts
@@ -4,7 +4,7 @@ import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from '../app-routing.module'; 
-import {AccordionModule} from 'primeng/accordion';  
+import {AccordionModule} from 'primeng/accordion'; 
 
 
 @NgModule({
